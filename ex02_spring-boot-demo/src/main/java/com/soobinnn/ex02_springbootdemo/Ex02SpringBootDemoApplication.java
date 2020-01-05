bffadb5726d3b1d6cee1459be05794c18c86b612
Soobinnn <isb9082@naver.com>
2020-01-05 16:31:27 +0900

    add spring-boot-demo project

---
+++ ex02_spring-boot-demo/src/main/java/com/soobinnn/ex02_springbootdemo/Ex02SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.soobinnn.ex02_springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex02SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex02SpringBootDemoApplication.class, args);
+	}
+
+}
---
+++ ex02_spring-boot-demo/src/test/java/com/soobinnn/ex02_springbootdemo/Ex02SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soobinnn.ex02_springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex02SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
