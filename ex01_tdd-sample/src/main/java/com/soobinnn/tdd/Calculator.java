abb79d8fa22428372b6969f949bd95f8e222d56d
Soobinnn <isb9082@naver.com>
2020-01-05 14:54:10 +0900

    add fibornacci test

--- ex01_tdd-sample/src/main/java/com/soobinnn/tdd/Calculator.java
+++ ex01_tdd-sample/src/main/java/com/soobinnn/tdd/Calculator.java
@@ -1,5 +1,7 @@
 package com.soobinnn.tdd;
 
+import java.util.stream.IntStream;
+
 public class Calculator {
     public int plus(int x, int y) {
         if(y == 0) {
@@ -18,11 +20,30 @@ public class Calculator {
     }
 
     public int multiply(int x, int y) {
-        int result = 0;
-        for (int i = 0; i < y; i++){
-            result += x;
+        //        int result = 0;
+        //        for (int i = 0; i < y; i++){
+        //            result += x;
+        //        }
+        //
+        //        return result;
+        //return IntStream.range(0, y).reduce(0,(a, e) -> a + x);
+
+        //return IntStream.iterate(0, i -> i+1).limit(y).reduce(0,(a, e) -> a + x);
+
+         return IntStream.generate(() -> 0).limit(y)
+                 .reduce(0,(a, e) -> plus(a, x));
+
+    }
+
+    public int fib(int x) {
+        if(x <= 0) {
+            return 0;
         }
 
-        return result;
+        if(x == 1 || x == 2) {
+            return 1;
+        }
+
+        return fib(x - 2) + fib(x - 1);
     }
 }
--- ex01_tdd-sample/src/test/java/com/soobinnn/tdd/CaculatorTest.java
+++ ex01_tdd-sample/src/test/java/com/soobinnn/tdd/CaculatorTest.java
@@ -9,11 +9,13 @@ package com.soobinnn.tdd;
 // lhs, rhs
 // lhs operator rhs
 // lhs = rhs
+// fibonacchi => 1 1 2 3 5 8 13 21 34
 
 // TDD
 // 1. Red
 // 2. Green
 // 3. Refactoring -> 1 + 2 -> x + y => Clean Code, 제약조건에 따라
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -22,21 +24,41 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class CaculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void plus() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.plus(2, 3), is(5));
     }
 
     @Test
     public void minus() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.minus(5, 3), is(2));
     }
 
     @Test
     public void multiply() {
-        Calculator calculator = new Calculator();
         assertThat(calculator.multiply(2,3), is(6));
+    }
+
+    @Test
+    public void fibonacciNormal() {
+        assertThat(calculator.fib(0), is( 0));
+        assertThat(calculator.fib(1),is(1));
+        assertThat(calculator.fib(1),is(1));
+        assertThat(calculator.fib(3),is(2));
+        assertThat(calculator.fib(4),is(3));
+        assertThat(calculator.fib(5),is(5));
+    }
+
+    @Test
+    public void fibonacciError() {
+        assertThat(calculator.fib(-1), is( 0));
     }
 }
